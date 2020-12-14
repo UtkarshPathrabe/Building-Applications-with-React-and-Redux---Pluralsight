dce773554a60a03b7e90120e5c9c39fc6852a7b5
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-14 20:46:47 +0530

    Update CoursesPage.js

--- Courses App/src/components/courses/CoursesPage.js
+++ Courses App/src/components/courses/CoursesPage.js
@@ -1,44 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
 class CoursesPage extends Component {
+	state = {
+		course: {
+			title: '',
+		},
+	};
 
-  state = {
-    course: {
-      title: ''
-    }
-  };
+	handleChange = (event) => {
+		const course = { ...this.state.course, title: event.target.value };
+		this.setState({ course });
+	};
 
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({course});
-  };
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.props.dispatch(courseActions.createCourse(this.state.course));
+		this.setState({
+			course: { ...this.state.course, title: '' },
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    alert(this.state.course.title);
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>
-          Courses
-        </h2>
-        <h3>
-          Add Course
-        </h3>
-        <input 
-          type='text' 
-          onChange={this.handleChange} 
-          value={this.state.course.title} 
-        />
-        <input 
-          type='submit' 
-          value='Save' 
-        />
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h2>Courses</h2>
+				<h3>Add Course</h3>
+				<input
+					type='text'
+					onChange={this.handleChange}
+					value={this.state.course.title}
+				/>
+				<input type='submit' value='Save' />
+				{this.props.courses.map((course) => (
+					<div key={course.title}>{course.title}</div>
+				))}
+			</form>
+		);
+	}
 }
 
-export default CoursesPage;
+CoursesPage.PropTypes = {
+	courses: PropTypes.array.isRequired,
+	dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+	return {
+		courses: state.courses,
+	};
+};
+
+export default connect(mapStateToProps)(CoursesPage);
