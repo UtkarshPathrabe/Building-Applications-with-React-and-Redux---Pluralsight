85b838fbdf33df07e6d95116182825b0c38df3ce
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-15 19:50:13 +0530

    Update courseActions.js

--- Courses App/src/redux/actions/courseActions.js
+++ Courses App/src/redux/actions/courseActions.js
@@ -1,15 +1,16 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export const createCourse = (course) => {
-	return {
-		type: types.CREATE_COURSE,
-		course,
-	};
-};
-
 const loadCoursesSuccess = (courses) => {
 	return { type: types.LOAD_COURSES_SUCCESS, courses };
+};
+
+const updateCourseSuccess = (course) => {
+	return { type: types.UPDATE_COURSE_SUCCESS, course };
+};
+
+const createCourseSuccess = (course) => {
+	return { type: types.CREATE_COURSE_SUCCESS, course };
 };
 
 export const loadCourses = () => {
@@ -18,6 +19,21 @@ export const loadCourses = () => {
 			.getCourses()
 			.then((courses) => {
 				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch((error) => {
+				throw error;
+			});
+	};
+};
+
+export const saveCourse = (course) => {
+	return function (dispatch) {
+		return courseApi
+			.saveCourse(course)
+			.then((savedCourse) => {
+				savedCourse.id
+					? dispatch(updateCourseSuccess(savedCourse))
+					: dispatch(createCourseSuccess(savedCourse));
 			})
 			.catch((error) => {
 				throw error;
